88d663de2212e6d7e3fb8199924e049145ac6601
Tobiloba Abiodun <abiodunzay@gmail.com>
2022-10-01 14:00:22 -0700

    i added my two files

---
+++ math1.py
@@ -0,0 +1,10 @@
+# 4x = 2x + 5
+
+whole_num = -4 + 5 
+divider = -2 
+x = whole_num / divider
+
+
+print(x)
+
+
---
+++ math2.py
@@ -0,0 +1,24 @@
+a = 5
+b = 7
+c = 10
+
+s = (a + b + c) /2
+
+pre_solution = s *(s-a) * (s-b) * (s-c)
+
+#or
+# d = ( s - a) 
+# e = ( s - b)
+# f = ( s - c)
+
+# g = s * d * e * f
+
+area = pre_solution ** 0.5
+
+print(area)
+
+
+
+
+
+
