35b773ac4a967acf42b021113c0f8de2af47bf26
Abiodun Zainab <abiodunzay@gmail.com>
2022-10-05 03:46:34 -0700

    Add files via upload

---
+++ ex_02.py
@@ -0,0 +1,49 @@
+# exercise 1
+# How many seconds are there in 42 minutes 42 seconds ?
+
+one_minute = 60
+given_minutes = 42
+given_seconds = 42
+number_of_seconds = one_minute * given_minutes + given_seconds
+
+print(number_of_seconds)
+
+
+# exercise 2
+# How many kilometers are there in 10 kilometers?
+
+one_kilometer = 1.61
+given_kilometer = 10
+number_of_kilometer = one_kilometer * given_kilometer
+
+print(number_of_kilometer)
+
+
+#exercise 3
+
+# ten_kilometer_race = 42
+# ten_kilometer_sec = 42
+# an_hour = 60
+# average_pace = ten_kilometer_race * an_hour 
+
+# print(average_pace)
+
+#exercise 4
+r= 5
+r = 4/3 * 3.142 * r**3
+
+print(r)
+
+
+#exercise 5 
+book_price = 24.95
+discount = 40/100
+shipping_first_copy = 3
+other_copy = 75
+
+
+
+
+
+
+
